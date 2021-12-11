27cb46131de299786e849ec122da2e4946839bb9
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2021-12-11 20:04:49 +0530

    Pics added

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -2,7 +2,7 @@ let Data = [
     {
         id: "1",
         name: "Tijo T Joseph",
-        img: "https://scontent-maa2-2.xx.fbcdn.net/v/t1.6435-9/182223615_1645431322318655_4908674799007918828_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=KYu5MEwecQgAX-qmHoa&_nc_ht=scontent-maa2-2.xx&oh=4666073f85fda6eacb97fb256a1d5d43&oe=61CF46E2",
+        img: "https://github.com/hello-world-ttj/MyProfile/blob/main/ttj.jpg?raw=true",
         day: 12,
         month: 3,
         year: 2000,
@@ -11,53 +11,73 @@ let Data = [
     },
     {
         id: "2",
-        name: "Lijo T Joseph",
-        img: "https://scontent-maa2-2.xx.fbcdn.net/v/t1.6435-9/182223615_1645431322318655_4908674799007918828_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=KYu5MEwecQgAX-qmHoa&_nc_ht=scontent-maa2-2.xx&oh=4666073f85fda6eacb97fb256a1d5d43&oe=61CF46E2",
-        day: 11,
-        month: 1,
+        name: "Adithyan S",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Adithyan.jpeg?raw=true",
+        day: 13,
+        month: 8,
         year: 2000,
-        insta: "https://www.instagram.com/hello_world.ttj/",
-        whtsp: "https://wa.me/918281977675"
+        insta: "https://www.instagram.com/_adithyan_s/",
+        whtsp: "https://wa.me/919061664028"
     },
     {
         id: "3",
-        name: "Sijo T Joseph",
-        img: "https://scontent-maa2-2.xx.fbcdn.net/v/t1.6435-9/182223615_1645431322318655_4908674799007918828_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=KYu5MEwecQgAX-qmHoa&_nc_ht=scontent-maa2-2.xx&oh=4666073f85fda6eacb97fb256a1d5d43&oe=61CF46E2",
-        day: 11,
+        name: "Malavika M",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Malavika.jpeg?raw=true",
+        day: 20,
         month: 1,
         year: 2000,
-        insta: "https://www.instagram.com/hello_world.ttj/",
-        whtsp: "https://wa.me/918281977675"
+        insta: "https://www.instagram.com/malavika1661/",
+        whtsp: "https://wa.me/917902302351"
     },
     {
         id: "4",
-        name: "Aijo T Joseph",
-        img: "https://scontent-maa2-2.xx.fbcdn.net/v/t1.6435-9/182223615_1645431322318655_4908674799007918828_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=KYu5MEwecQgAX-qmHoa&_nc_ht=scontent-maa2-2.xx&oh=4666073f85fda6eacb97fb256a1d5d43&oe=61CF46E2",
-        day: 11,
-        month: 1,
+        name: "Anaswara S",
+        img: "",
+        day: 14,
+        month: 6,
         year: 2000,
-        insta: "https://www.instagram.com/hello_world.ttj/",
-        whtsp: "https://wa.me/918281977675"
+        insta: "https://www.instagram.com/am_anuzzzzz/",
+        whtsp: "https://wa.me/919567714695"
     },
     {
         id: "5",
-        name: "Bijo T Joseph",
-        img: "https://scontent-maa2-2.xx.fbcdn.net/v/t1.6435-9/182223615_1645431322318655_4908674799007918828_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=KYu5MEwecQgAX-qmHoa&_nc_ht=scontent-maa2-2.xx&oh=4666073f85fda6eacb97fb256a1d5d43&oe=61CF46E2",
-        day: 12,
-        month: 3,
+        name: "Sreenath N",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Sreenath.jpeg?raw=true",
+        day: 28,
+        month: 4,
         year: 2000,
-        insta: "https://www.instagram.com/hello_world.ttj/",
-        whtsp: "https://wa.me/918281977675"
+        insta: "https://www.instagram.com/sree_musicz/",
+        whtsp: "https://wa.me/917025098714"
     },
     {
         id: "6",
-        name: "Cijo T Joseph",
-        img: "https://scontent-maa2-2.xx.fbcdn.net/v/t1.6435-9/182223615_1645431322318655_4908674799007918828_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=KYu5MEwecQgAX-qmHoa&_nc_ht=scontent-maa2-2.xx&oh=4666073f85fda6eacb97fb256a1d5d43&oe=61CF46E2",
-        day: 12,
-        month: 3,
+        name: "Avinash",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Avinash.jpeg?raw=true",
+        day: 4,
+        month: 5,
         year: 2000,
-        insta: "https://www.instagram.com/hello_world.ttj/",
-        whtsp: "https://wa.me/918281977675"
+        insta: "https://www.instagram.com/_itz__billy__/",
+        whtsp: "https://wa.me/919207211929"
+    },
+    {
+        id: "7",
+        name: "Nandana",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Nandana.jpeg?raw=true",
+        day: 23,
+        month: 5,
+        year: 2000,
+        insta: "https://www.instagram.com/_nandanasuresh/",
+        whtsp: "https://wa.me/919947258097"
+    },
+    {
+        id: "8",
+        name: "Aiswarya P Nair",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Aiswarya.jpeg?raw=true",
+        day: 17,
+        month: 4,
+        year: 2001,
+        insta: "https://www.instagram.com/aiswarya.p.nair_/",
+        whtsp: "https://wa.me/918078190645"
     }
 ]
 
