cda5207ecb55674207e665df9805a2c033f60b92
Tijo T Joseph <tijotjoseph@Tijos-MacBook-Air.local>
2021-12-12 11:44:04 +0530

    New Data

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,16 +1,6 @@
 let Data = [
     {
         id: "1",
-        name: "Tijo T Joseph",
-        img: "https://github.com/hello-world-ttj/MyProfile/blob/main/ttj.jpg?raw=true",
-        day: 12,
-        month: 3,
-        year: 2000,
-        insta: "https://www.instagram.com/hello_world.ttj/",
-        whtsp: "https://wa.me/918281977675"
-    },
-    {
-        id: "2",
         name: "Adithyan S",
         img: "https://github.com/hello-world-ttj/MCA/blob/main/Adithyan.jpeg?raw=true",
         day: 13,
@@ -20,57 +10,7 @@ let Data = [
         whtsp: "https://wa.me/919061664028"
     },
     {
-        id: "3",
-        name: "Malavika M",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Malavika.jpeg?raw=true",
-        day: 20,
-        month: 1,
-        year: 2000,
-        insta: "https://www.instagram.com/malavika1661/",
-        whtsp: "https://wa.me/917902302351"
-    },
-    {
-        id: "4",
-        name: "Anaswara S",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Anaswara.jpeg?raw=true",
-        day: 14,
-        month: 6,
-        year: 2000,
-        insta: "https://www.instagram.com/am_anuzzzzz/",
-        whtsp: "https://wa.me/919567714695"
-    },
-    {
-        id: "5",
-        name: "Sreenath N",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Sreenath.jpeg?raw=true",
-        day: 28,
-        month: 4,
-        year: 2000,
-        insta: "https://www.instagram.com/sree_musicz/",
-        whtsp: "https://wa.me/917025098714"
-    },
-    {
-        id: "6",
-        name: "Avinash",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Avinash.jpeg?raw=true",
-        day: 4,
-        month: 5,
-        year: 2000,
-        insta: "https://www.instagram.com/_itz__billy__/",
-        whtsp: "https://wa.me/919207211929"
-    },
-    {
-        id: "7",
-        name: "Nandana",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Nandana.jpeg?raw=true",
-        day: 23,
-        month: 5,
-        year: 2000,
-        insta: "https://www.instagram.com/_nandanasuresh/",
-        whtsp: "https://wa.me/919947258097"
-    },
-    {
-        id: "8",
+        id: "2",
         name: "Aiswarya P Nair",
         img: "https://github.com/hello-world-ttj/MCA/blob/main/Aiswarya.jpeg?raw=true",
         day: 17,
@@ -80,27 +20,7 @@ let Data = [
         whtsp: "https://wa.me/918078190645"
     },
     {
-        id: "9",
-        name: "Anjali J P",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Anjali.jpeg?raw=true",
-        day: 31,
-        month: 3,
-        year: 2000,
-        insta: "https://www.instagram.com/anjalijp297/",
-        whtsp: "https://wa.me/918590039587"
-    },
-    {
-        id: "10",
-        name: "Printo P Varghese",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Printo.jpeg?raw=true",
-        day: 11,
-        month: 10,
-        year: 2000,
-        insta: "https://www.instagram.com/mr_printo_007/",
-        whtsp: "https://wa.me/917012175260"
-    },
-    {
-        id: "11",
+        id: "3",
         name: "Akhil Krishna S",
         img: "https://github.com/hello-world-ttj/MCA/blob/main/Akhil.jpeg?raw=true",
         day: 17,
@@ -110,28 +30,8 @@ let Data = [
         whtsp: "https://wa.me/918943684583"
     },
     {
-        id: "12",
-        name: "Nahas",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Nahas.jpeg?raw=true",
-        day: 13,
-        month: 12,
-        year: 2000,
-        insta: "https://www.instagram.com/_i_am_nahas/",
-        whtsp: "https://wa.me/917025094262"
-    },
-    {
-        id: "13",
-        name: "Ameer",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Ameer.jpeg?raw=true",
-        day: 17,
-        month: 1,
-        year: 2000,
-        insta: "https://www.instagram.com/ameer_4_2/",
-        whtsp: "https://wa.me/919745476784"
-    },
-    {
-        id: "14",
-        name: "Akhil",
+        id: "4",
+        name: "Akhil Suresh",
         img: "https://github.com/hello-world-ttj/MCA/blob/main/AkhilS.jpeg?raw=true",
         day: 2,
         month: 12,
@@ -140,57 +40,7 @@ let Data = [
         whtsp: "https://wa.me/919605274910"
     },
     {
-        id: "15",
-        name: "Aswin Asok",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Aswin.jpeg?raw=true",
-        day: 28,
-        month: 3,
-        year: 1998,
-        insta: "https://www.instagram.com/aswinasok97/",
-        whtsp: "https://wa.me/917736676311"
-    },
-    {
-        id: "16",
-        name: "Melvin Thomas",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Melvin.jpeg?raw=true",
-        day: 30,
-        month: 10,
-        year: 1999,
-        insta: "https://www.instagram.com/melvin_thomzz/",
-        whtsp: "https://wa.me/917902968636"
-    },
-    {
-        id: "17",
-        name: "Sambhu Muralidharan",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Sambhu.jpeg?raw=true",
-        day: 22,
-        month: 10,
-        year: 1996,
-        insta: "https://www.instagram.com/the_mind_catcher__/",
-        whtsp: "https://wa.me/918281763143"
-    },
-    {
-        id: "18",
-        name: "Sandhra Murali M",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Sandhra.jpeg?raw=true",
-        day: 10,
-        month: 11,
-        year: 2000,
-        insta: "https://www.instagram.com/lime_.lght/",
-        whtsp: "https://wa.me/918606578487"
-    },
-    {
-        id: "19",
-        name: "Ananthu R Nair",
-        img: "https://github.com/hello-world-ttj/MCA/blob/main/Ananthu.jpeg?raw=true",
-        day: 19,
-        month: 9,
-        year: 2000,
-        insta: "https://www.instagram.com/mojo_b0y/",
-        whtsp: "https://wa.me/919946779289"
-    },
-    {
-        id: "20",
+        id: "8",
         name: "Amala Joseph",
         img: "https://github.com/hello-world-ttj/MCA/blob/main/Amala.jpeg?raw=true",
         day: 30,
@@ -200,7 +50,87 @@ let Data = [
         whtsp: "https://wa.me/917902408329"
     },
     {
-        id: "21",
+        id: "013",
+        name: "Ameer Shaji",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Ameer.jpeg?raw=true",
+        day: 17,
+        month: 1,
+        year: 2000,
+        insta: "https://www.instagram.com/ameer_4_2/",
+        whtsp: "https://wa.me/919745476784"
+    },
+    {
+        id: "10",
+        name: "Ananthu R Nair",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Ananthu.jpeg?raw=true",
+        day: 19,
+        month: 9,
+        year: 2000,
+        insta: "https://www.instagram.com/mojo_b0y/",
+        whtsp: "https://wa.me/919946779289"
+    },
+    {
+        id: "11",
+        name: "Anaswara S",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Anaswara.jpeg?raw=true",
+        day: 14,
+        month: 6,
+        year: 2000,
+        insta: "https://www.instagram.com/am_anuzzzzz/",
+        whtsp: "https://wa.me/919567714695"
+    },
+    {
+        id: "12",
+        name: "Anjali J P",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Anjali.jpeg?raw=true",
+        day: 31,
+        month: 3,
+        year: 2000,
+        insta: "https://www.instagram.com/anjalijp297/",
+        whtsp: "https://wa.me/918590039587"
+    },
+    {
+        id: "14",
+        name: "Aswathi P Raj",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Aswathi.jpg?raw=true",
+        day: 16,
+        month: 1,
+        year: 2001,
+        insta: "https://www.instagram.com/aswathirajeev._/",
+        whtsp: "https://wa.me/919946721934"
+    },
+    {
+        id: "16",
+        name: "Aswin Asok",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Aswin.jpeg?raw=true",
+        day: 28,
+        month: 3,
+        year: 1998,
+        insta: "https://www.instagram.com/aswinasok97/",
+        whtsp: "https://wa.me/917736676311"
+    },
+    {
+        id: "18",
+        name: "Avinash Unni",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Avinash.jpeg?raw=true",
+        day: 4,
+        month: 5,
+        year: 2000,
+        insta: "https://www.instagram.com/_itz__billy__/",
+        whtsp: "https://wa.me/919207211929"
+    },
+    {
+        id: "34",
+        name: "Malavika M",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Malavika.jpeg?raw=true",
+        day: 20,
+        month: 1,
+        year: 2000,
+        insta: "https://www.instagram.com/malavika1661/",
+        whtsp: "https://wa.me/917902302351"
+    },
+    {
+        id: "35",
         name: "Megha Manoj",
         img: "https://github.com/hello-world-ttj/MCA/blob/main/Megha.jpeg?raw=true",
         day: 27,
@@ -208,6 +138,86 @@ let Data = [
         year: 2000,
         insta: "https://www.instagram.com/_megha_manoj___/",
         whtsp: "https://wa.me/918157809128"
+    },
+    {
+        id: "36",
+        name: "Melvin Thomas",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Melvin.jpeg?raw=true",
+        day: 30,
+        month: 10,
+        year: 1999,
+        insta: "https://www.instagram.com/melvin_thomzz/",
+        whtsp: "https://wa.me/917902968636"
+    },
+    {
+        id: "39",
+        name: "N Nahas",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Nahas.jpeg?raw=true",
+        day: 13,
+        month: 12,
+        year: 2000,
+        insta: "https://www.instagram.com/_i_am_nahas/",
+        whtsp: "https://wa.me/917025094262"
+    },
+    {
+        id: "40",
+        name: "Nandana Suresh",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Nandana.jpeg?raw=true",
+        day: 23,
+        month: 5,
+        year: 2000,
+        insta: "https://www.instagram.com/_nandanasuresh/",
+        whtsp: "https://wa.me/919947258097"
+    },
+    {
+        id: "44",
+        name: "Printo P Varghese",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Printo.jpeg?raw=true",
+        day: 11,
+        month: 10,
+        year: 2000,
+        insta: "https://www.instagram.com/mr_printo_007/",
+        whtsp: "https://wa.me/917012175260"
+    },
+    {
+        id: "51",
+        name: "Sambhu Muralidharan",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Sambhu.jpeg?raw=true",
+        day: 22,
+        month: 10,
+        year: 1996,
+        insta: "https://www.instagram.com/the_mind_catcher__/",
+        whtsp: "https://wa.me/918281763143"
+    },
+    {
+        id: "52",
+        name: "Sandhra Murali M",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Sandhra.jpeg?raw=true",
+        day: 10,
+        month: 11,
+        year: 2000,
+        insta: "https://www.instagram.com/lime_.lght/",
+        whtsp: "https://wa.me/918606578487"
+    },
+    {
+        id: "58",
+        name: "Sreenath N",
+        img: "https://github.com/hello-world-ttj/MCA/blob/main/Sreenath.jpeg?raw=true",
+        day: 28,
+        month: 4,
+        year: 2000,
+        insta: "https://www.instagram.com/sree_musicz/",
+        whtsp: "https://wa.me/917025098714"
+    },
+    {
+        id: "59",
+        name: "Tijo T Joseph",
+        img: "https://github.com/hello-world-ttj/MyProfile/blob/main/ttj.jpg?raw=true",
+        day: 12,
+        month: 3,
+        year: 2000,
+        insta: "https://www.instagram.com/hello_world.ttj/",
+        whtsp: "https://wa.me/918281977675"
     }
 ]
 
